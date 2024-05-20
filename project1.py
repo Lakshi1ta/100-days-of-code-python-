3ae399ecef70ac3ea14d1f790a1b1f0039b39473
Lakshita Kandpal <149196032+Lakshi1ta@users.noreply.github.com>
2024-05-20 21:17:16 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      kandp
+#
+# Created:     20-05-2024
+# Copyright:   (c) kandp 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print("Welcome to the Band name generator.");
+city=input("What's the name of your city you grew up in? \n");
+pet=input("What's your pet's name?\n");
+print("Your band name could be ",city+ " " +pet);
